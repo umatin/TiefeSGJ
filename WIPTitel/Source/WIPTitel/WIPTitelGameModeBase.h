9d3ece1da0b35d76a62283ed7768a508687c29d5
umatin <martin.eppert@web.de>
2017-06-09 23:11:57 +0200

    Lets go boid

---
+++ WIPTitel/Source/WIPTitel/WIPTitelGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WIPTitelGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WIPTITEL_API AWIPTitelGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
